ba3435afa524a6e74df97a4c26e2bff811652fa5
Camila <camyalexrc@gmail.com>
2023-04-20 00:02:22 -0400

    Se sube repositorio

---
+++ src/main/java/Clases/claseConexion.java
@@ -0,0 +1,31 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class claseConexion {
+    Connection conexion;
+    
+    //credenciales
+    String correo="root";
+    String contrasena="";
+    String bd="proyectowyg";
+    String ip="localhost";
+    String puerto="3306";
+    
+    String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    //metodo de validación de conexión
+    public Connection establecerConexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(cadena, correo, contrasena);
+            //JOptionPane.showMessageDialog(null,"Conexión exitosa!");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Problemas de conexion"+ e.toString());
+        }
+        return conexion;
+    }
+}
---
+++ src/main/java/com/mycompany/proyectowyg/ProyectoWYG.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectowyg;
+
+/**
+ *
+ * @author Acer
+ */
+public class ProyectoWYG {
+
+    public static void main(String[] args) {
+        //Visualización
+        index objetoIndex = new index();
+        objetoIndex.setVisible(true);
+    }
+}
